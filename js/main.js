4d8d80da7e0d8361947e99afc957ebf29e2ae2c2
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2022-11-10 16:16:08 -0800

    Added comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// pfproulette javascript code
+// By Christopher Rempe, 2022
+
 var dir = './images/'
 var fileExtension = '.jpg'
 var picture = ''
@@ -6,6 +9,7 @@ function getRandomInt(max, min) {
     return Math.floor(Math.random() * (max - min) + min);
   }
 
+// When the site loads, choose a random image for the background
 $(document).ready(function () {
     var num1 = getRandomInt(4, 0) + 1
     console.log(num1)
@@ -13,6 +17,8 @@ $(document).ready(function () {
 
 })
 
+// Start the spin! Gets the first displayed photo, then calls spinRandom with
+// a random number of spins
 function spin() {
   var spinPhoto1 = dir + 'picture' + (getRandomInt(4, 0) + 1) + fileExtension
   $('#spin-box').append($('<img>',{id:'img1',class:'profileImg',src:spinPhoto1}))
@@ -23,21 +29,28 @@ function spin() {
   }, 250)
 }
 
+// Get a random image and put it on the screen spinsLeft number of times
 function spinRandom(spinsLeft) {
-  console.log(spinsLeft)
+
+  // Create the id for the image, get a random image, and add it to the DOM
   var imgId = "img" + spinsLeft
-  console.log(imgId)
   picture = dir + 'picture' + (getRandomInt(4, 0) + 1) + fileExtension
   $('#spin-box').append($('<img>',{id:imgId,class:'profileImg',src:picture}))
   spinsLeft = spinsLeft - 1
+
+  // Wait for 350ms before having image pop up
   setTimeout(function () {
     $('#'+imgId).css("margin-top", "0")
+
+    // If there are more spins left, repeat
     if (spinsLeft > 0) {
       spinRandom(spinsLeft)
     }
   },350)
 }
 
+// When the user clicks the first spin button, bring up the spin container,
+// then spin
 function firstSpin() {
   $('#spin-container').addClass('spin-container-up')
   setTimeout(function () {
